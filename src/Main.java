c5ea0c1c3c2a1296904785a2cb106036dac94604
Zuko <dani4200256@gmail.com>
2022-09-20 17:13:27 +0300

    New

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Match match = new Match();
-        System.out.println(match.tidyNumber(12));
+        System.out.println(match.tidyNumber(scanner.nextInt()));
     }
 
 
